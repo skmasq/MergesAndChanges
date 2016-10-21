e811219696d88b2314738cb6f7405e39669688db
skmasq <Qpem1337>
2016-10-21 15:09:00 +0100

    Fixed naming

--- MergesAndChanges/Controllers/FeatureController.cs
+++ MergesAndChanges/Controllers/FeatureController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MergesAndChanges.Controller
+namespace MergesAndChanges.Controllers
 {
     public class FeatureController : Controller
     {
@@ -14,5 +14,7 @@ namespace MergesAndChanges.Controller
         {
             return View();
         }
+
+
 	}
 }
