0b54765cceaf0f5e1b94ecb7af52570c924bf712
skmasq <Qpem1337>
2016-10-21 14:59:19 +0100

    Initial setup

---
+++ MergesAndChanges/Controller/FeatureController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MergesAndChanges.Controller
+{
+    public class FeatureController : Controller
+    {
+        //
+        // GET: /Feature/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
