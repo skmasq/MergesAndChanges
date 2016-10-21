d36398de4cf2f76119657e5f5eb7e9582924fb8e
skmasq <Qpem1337>
2016-10-21 15:35:25 +0100

    Added new scenario

---
+++ MergesAndChanges/Controllers/AnotherFeatureController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MergesAndChanges.Controllers
+{
+    public class AnotherFeatureController : Controller
+    {
+        //
+        // GET: /AnotherFeature/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
